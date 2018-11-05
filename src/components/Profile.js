def7eed854d54bff621cc15e280922575949d076
Angelvilad <angelvilad@hotmail.com>
2018-11-05 21:53:39 +0100

    Improve Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const ProfileView = (props) =>
-    <div className="Profile-wrapper">
-        <p>Profile</p>   
-        {/*<img src={author.picture.medium} />*/}
-    </div>
+import fullName from '../utils/utils';
+
+const ProfileView = (props) => {
+    const [author] = props.authors.authors.filter(author =>
+        author.login.uuid === props.match.params.uuidAuthor);
+    return (
+        <div className="Profile-wrapper">
+            <h2>Perfil de <span className="full-name">{fullName(author)}</span></h2> 
+            <img src={author.picture.large} />
+        </div>
+    );
+}
+
 
 const Profile = connect(
     state => state
