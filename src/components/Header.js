7020ca207e20853e637d68ccfdfaeb41963dd049
Angelvilad <angelvilad@hotmail.com>
2018-11-23 13:46:25 +0100

    Styling Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import styled from 'styled-components';
 
 import { doLogOut } from '../store/actions/login';
 
 const HeaderView = (props) =>
-  <header>
+  <header className={props.className}>
     <div className="main-title">
       <h1><Link to="/">SocialBook</Link></h1>
     </div>    
@@ -17,8 +18,7 @@ const HeaderView = (props) =>
           }
           {props.login.logged && 
             <React.Fragment>
-              <li>
-                <Link to={`/profile/${props.login.user.login.uuid}`}>My Profile</Link></li>
+              <li><Link to={`/profile/${props.login.user.login.uuid}`}>My Profile</Link></li>
               <li><Link to="/" onClick={props.logOut}>Logout</Link></li>
             </React.Fragment>            
           }
@@ -36,4 +36,22 @@ const HeaderView = (props) =>
     })
   )(HeaderView);
 
-  export default Header;
+  export default styled(Header)`
+    background-color: ${({theme}) => theme.colors.main};
+    min-height: 89px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 ${({theme}) => theme.padding.gutter};
+    & a {
+      text-decoration: none;
+      color: white;
+    }
+    & ul {
+      display: flex;
+      list-style: none;
+    }
+    & li {
+      padding: 0 ${({theme}) => theme.padding.gutter};
+    }
+  `;
